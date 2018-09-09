4c20b8bb65bf071f88e2d5694caf9331df4307cf
Mikouhero <290468335@qq.com>
2018-09-09 12:22:19 +0800

    tag

--- public/static/admin/js/admin/category.js
+++ public/static/admin/js/admin/category.js
@@ -4,9 +4,13 @@ vm = new Vue({
         menuList: '',
         addName:'',
         addUrl:'',
-        addStatus:'',
+        addStatus:1,
         addParentname:'',
-        addParentid:''
+        addParentid:'',
+        addSort:1,
+        editMsg: Array(),
+        deleteId:''
+
     },
     methods: {
         getHomeMenu:function () {
@@ -19,7 +23,6 @@ vm = new Vue({
                     return false;
                 }
                 this.menuList = res.data.data;
-                console.log(this.menuList);
             }, function (res) {
                 alert(res);
             });
@@ -29,8 +32,77 @@ vm = new Vue({
             this.addParentid= id;
         },
         addMenu:function () {
-            
+            this.$http.post(ajaxUrl.addMenu, {
+                name:this.addName,
+                url: this.addUrl,
+                sort :this.addSort,
+                status:Number(this.addStatus),
+                parent_id:this.addParentid
+            }, {
+                emulateJSON: true
+            }).then(function (res) {
+                if (res.data.code != 200) {
+                    alert(res.data.msg);
+                    return false;
+                }
+                this.addName='';
+                this.addUrl='';
+                this.addSort=1;
+                this.addSatus=1;
+                this.addParentname='';
+                this.addParentid='';
+                this.getHomeMenu();
+
+            }, function (res) {
+            });
+        },
+        editaddStatus:function () {
+            this.addSatus= this.addSatus || 1;
+        },
+        editMenutmp:function (key,k) {
+            if(k == 'tmp'){
+                this.menuList[key].child='';
+                this.editMsg = this.menuList[key];
+
+            }else{
+                this.editMsg = this.menuList[key].child[k];
+            }
+        },
+        editMenu:function () {
+           this.editMsg['status'] = Number(this.editMsg['status']);
+
+            this.$http.post(ajaxUrl.editMenu, {
+               msg:this.editMsg
+            }, {
+                emulateJSON: true
+            }).then(function (res) {
+                if (res.data.code != 200) {
+                    alert(res.data.msg);
+                    return false;
+                }
+                this.editMsg=[];
+                this.getHomeMenu();
+            }, function (res) {
+            });
+        },
+        deleteMenu: function (id) {
+            this.deleteId = id;
+            this.$http.post(ajaxUrl.deleteMenu, {
+                id:this.deleteId
+            }, {
+                emulateJSON: true
+            }).then(function (res) {
+                if (res.data.code != 200) {
+                    alert(res.data.msg);
+                    return false;
+                }
+                this.deleteId = '';
+                this.getHomeMenu();
+
+            }, function (res) {
+            });
         }
+
     },
     mounted: function () {
         this.$nextTick(function () {
--- public/static/admin/js/common.js
+++ public/static/admin/js/common.js
@@ -1,5 +1,11 @@
 var url = 'http://tp5.com/admin';
 var ajaxUrl = {
     getMenuList: url + '/category/getMenuList',
-
+    addMenu: url + '/category/addMenu',
+    editMenu: url + '/category/editMenu',
+    deleteMenu: url + '/category/deleteMenu',
+    getTagList:url + '/tag/getTagList',
+    addTag: url + '/tag/addTag',
+    banTag:url + "/tag/banTag",
+    openTag:url + "/tag/openTag",
 };
